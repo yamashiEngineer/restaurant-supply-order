ee13e8c9c77100345a4e5f720ffda5cca5a78449
Takaomi Yamashita <ty.8479@icloud.com>
2026-02-03 22:47:20 +0900

    Thymeleafの共通化

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -5,18 +5,23 @@ import com.example.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/users")
+@Controller
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class UserController {
 
   private final UserRepository userRepository;
 
   @GetMapping
+  public String showLogin() {
+    return "login";
+  }
+
+  @GetMapping("/users")
   public List<Users> getAllUsers() {
     var users = userRepository.findAll();
     return users;
